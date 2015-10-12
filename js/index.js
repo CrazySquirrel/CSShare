a46e6665bfc238ea0b329e4d46461b02deca9c62
CrazySquirrel <admin@crazysquirrel.ru>
2015-10-13 02:13:19 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".cstiles").CSTiles({
+		shareTile: true,
+		shareTypes: ["facebook","vkontakte","email","print"]		
+	});
+	/*
+	$(".cstiles__item-content").CSShare({
+		types: ["facebook","vkontakte","email","print"]	
+	});
+	*/
+});
